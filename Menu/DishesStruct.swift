8dc65ec12188379ef8a6e0569a03b3843048c2e0
karbawona <37221123+karbawona@users.noreply.github.com>
2019-02-10 20:24:38 +0100

    i did it! json from local file is done

--- Menu/DishesStruct.swift
+++ Menu/DishesStruct.swift
@@ -16,15 +16,7 @@ struct Dish : Codable {
     var category : String
     let preparationTime : String
     
-    init(n: String, cat: String, prTime: String) {
-        name = n
-        category = cat
-        preparationTime = prTime
-    }
     
-    
- 
-
 }
 
 
@@ -32,55 +24,9 @@ struct Dish : Codable {
 struct Dishes : Codable {
     
     let dishesStruct : [Dish]
-}
-
-
-func loadJsonmm() -> [Dish]? {
-    if let url = Bundle.main.url(forResource: "data", withExtension: "json") {
-        
-        do {
-            let data = try Data(contentsOf: url)
-            let decoder = JSONDecoder()
-            let jsonData = try decoder.decode(Dishes.self, from: data)
-            print (jsonData.dishesStruct)
-            return jsonData.dishesStruct
-        } catch {
-            print("error:\(error)")
-        }
-    }
-    return nil
-}
-
-//func getFromFile()
-//{
-//    let path: NSString = Bundle.main.path(forResource: "days",
-//                                          ofType: "json")! as NSString
-//    let data : NSData = try! NSData(contentsOfFile: path as String,
-//                                    options: NSData.ReadingOptions.dataReadingMapped)
-//    
-//    let dict: NSDictionary!=(try! JSONSerialization.jsonObject(with: data as Data, options: JSONSerialization.ReadingOptions.mutableContainers)) as? NSDictionary
-//    
-//    for i in 0..<(dict.value(forKey: "Marvel") as! NSArray).count
-//    {
-//        arrayDict.add((dict.value(forKey: "Marvel") as! NSArray).object(at: i))
-//    }
-//    for i in 0..<(dict.value(forKey: "DC") as! NSArray).count
-//    {
-//        arrayDict.add((dict.value(forKey: "DC") as! NSArray).object(at: i))
-//    }
-//   // newTableView.reloadData()
-//}
-//
-
-extension Decodable {
-    
-    static func fromJSON<T:Decodable>(_ fileName: String, fileExtension: String="json", bundle: Bundle = .main) throws -> T {
-        guard let url = bundle.url(forResource: fileName, withExtension: fileExtension) else {
-            throw NSError(domain: NSURLErrorDomain, code: NSURLErrorResourceUnavailable)
-        }
-        
-        let data = try Data(contentsOf: url)
-        
-        return try JSONDecoder().decode(T.self, from: data)
+    enum CodingKeys : String, CodingKey {
+        case dishesStruct = "dishes"
     }
 }
+
+
--- Menu/ViewController.swift
+++ Menu/ViewController.swift
@@ -17,23 +17,34 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        let  path = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("saving.json")
-        //print(path)
+     //   print(path)
         
         do{
            let input = try String (contentsOf: path!)
-        print (input)
+       //     print (input)
+            let contentData = input.data(using: .utf8)
+          
+            let jsDec = JSONDecoder()
+            do {
+            data = try jsDec.decode(Dishes.self, from: contentData!)
+           //     print (data)
+            } catch {
+                print ("nie dziala data")
+                print(error)
+            }
         } catch{ print (error) }
    
+        for index in 0...3 {
+            print("category: \(String(describing: data?.dishesStruct[index].category))")
+            print("name: \(String(describing: data?.dishesStruct[index].name))")
+            print("time: \(String(describing: data?.dishesStruct[index].preparationTime))")
+        }
+        
     }
     
 
-
     
-    
-    
-    
-    
-    
+   
     
     
     @IBOutlet weak var drawedDish: UITextView!
